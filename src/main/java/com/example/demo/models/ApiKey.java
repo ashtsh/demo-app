9febd2cd24c47a5766b0625c5a8c53ec018d4c37
ashutosh <ashumrsingh@gmail.com>
2021-09-06 10:24:52 +0530

    pushing the project

---
+++ src/main/java/com/example/demo/models/ApiKey.java
@@ -0,0 +1,26 @@
+package com.example.demo.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class ApiKey {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String apiKey;
+	public String getKey() {
+		return apiKey;
+	}
+	public void setKey(String key) {
+		apiKey = key;
+	}
+	public Long getId() {
+		return id;
+	}
+	
+}
