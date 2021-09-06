b968f476f97079f70eec9fb88a5d1d045fcc0b84
ashtsh <ashumrsingh@gmail.com>
2021-09-06 14:55:45 +0530

    organizing backend and frontend folder structure

---
+++ demo-app-backend/src/main/java/com/example/demo/controllers/YTController.java
@@ -0,0 +1,75 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.jpa.Search;
+import org.hibernate.search.query.dsl.QueryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.models.ApiKey;
+import com.example.demo.models.YoutubeMetadata;
+import com.example.demo.repositories.ApiKeyRepository;
+import com.example.demo.services.ApiKeyService;
+import com.example.demo.services.YTMetadataService;
+
+@RestController
+public class YTController {
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	@Autowired
+	private YTMetadataService yt_service;
+	@Autowired
+	private ApiKeyService key_service;
+	@Autowired
+	private ApiKeyRepository repo;
+	
+	@GetMapping("/get")
+	public Page<YoutubeMetadata> getFirstPageData(){
+		return yt_service.getYTMetadata(0);
+	}
+	@GetMapping("/{pageNo}")
+	private Page<YoutubeMetadata> getNextPagesData(@PathVariable int pageNo){
+		return yt_service.getYTMetadata(pageNo);
+	}
+	
+	@GetMapping("/test")
+	private void test() {
+		ApiKey a = new ApiKey();
+		a.setKey("hello world");
+		repo.save(a);
+	}
+	
+	@GetMapping("/add-key")
+	private void addApiKey(@RequestParam String key) {
+		key_service.addNewKey(key);
+	}
+	
+	@GetMapping("/search")
+	private List<YoutubeMetadata> searchController(@RequestParam(name="query") String searchString) {
+		FullTextEntityManager fullTextEntityManager 
+		  = Search.getFullTextEntityManager(entityManager);
+		QueryBuilder queryBuilder = fullTextEntityManager.getSearchFactory() 
+				  .buildQueryBuilder()
+				  .forEntity(YoutubeMetadata.class)
+				  .get();
+		org.apache.lucene.search.Query query = queryBuilder
+				  .keyword()
+				  .onFields("title","description")
+				  .matching(searchString)
+				  .createQuery();
+		org.hibernate.search.jpa.FullTextQuery jpaQuery
+		  = fullTextEntityManager.createFullTextQuery(query, YoutubeMetadata.class);
+		return jpaQuery.getResultList();
+	}
+	
+}
